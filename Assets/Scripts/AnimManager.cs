c26f5cbb01fc729d2b1fd5b8a23ab87c5d5e1d27
krongle <kcunliffe176@gmail.com>
2025-10-31 16:41:20 +0000

    Animation implementing(not complete)

---
+++ Assets/Scripts/AnimManager.cs
@@ -0,0 +1,22 @@
+using UnityEditorInternal;
+using UnityEngine;
+//using UnityEditor.Animations.AnimatorController;
+
+public class AnimManager : MonoBehaviour
+{
+   // private Elephant_Controller;
+    private 
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
