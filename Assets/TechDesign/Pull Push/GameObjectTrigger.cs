1d872c009974c8e07d9e5de272ca581f68fcea69
Wizard <cb10crt@bolton.ac.uk>
2025-11-06 00:07:15 +0000

    Push Prefab & ObjTrigger Change
    Adding the push puzzle prefab.
    Adding two lines to the object trigger that destroy and object, will be changed to open a gate or something similar when those assets are developed.

---
+++ Assets/TechDesign/Pull Push/GameObjectTrigger.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class GameObjectTrigger : MonoBehaviour
+{
+    [Header("This object will trigger the trigger")]
+    [Tooltip("Make sure triggerObject has a rigidbody")]
+    public GameObject triggerObject;
+
+    public bool Trigger;
+
+    //Temp
+    public GameObject destroyObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == triggerObject)
+        {
+            Trigger = true;
+
+            //Temp
+            Destroy(destroyObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == triggerObject)
+        {
+            Trigger = false;
+        }
+    }
+}
