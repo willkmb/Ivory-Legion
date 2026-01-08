4439717fb660dd287ac1e1bfd0bb9b06728ded15
TommyDeluce <td2crt@bolton.ac.uk>
2026-01-08 17:35:49 +0000

    Fixed push/pull collision and out of bounds issues
    Disabling the box collider on the player fixed the collision problem of having the push/pull objects merge with other objects when pushed against them.
    
    I also added multiple raycasts to the push/pull objects so that when a side is too close to a barrier, that side cannot be pushed from, this fixes a problem where the was able to latch onto a side that was against a barrier and break out of bounds.

--- Assets/TechDesign/Pull Push/Remake/PushPull_Remake.cs
+++ Assets/TechDesign/Pull Push/Remake/PushPull_Remake.cs
@@ -11,10 +11,32 @@ public class PushPull_Remake : MonoBehaviour
     public GameObject Player;
 
     public GameObject MoveableObj;
-    //PushPull_Remake self;
-    //public PushPull_Remake MainPushObj;
-    //public PushPull_Remake nearestPushable;
-    //public PushPull_Remake[] allPushObjects;
+
+    //public GameObject[] Raycasts;
+
+    public GameObject[] Ray1;
+    public GameObject[] Ray2;
+    public GameObject[] Ray3;
+    public GameObject[] Ray4;
+
+    //private GameObject[] HitRays;
+
+
+    public float range = 0;
+    bool hasHit;
+
+    [SerializeField] private GameObject hitObj1;
+    [SerializeField] private GameObject hitObj2;
+    [SerializeField] private GameObject hitObj3;
+    [SerializeField] private GameObject hitObj4;
+
+    [SerializeField] public TriggerScript_PlayerCheck triggerObj1;
+    [SerializeField] public TriggerScript_PlayerCheck triggerObj2;
+    [SerializeField] public TriggerScript_PlayerCheck triggerObj3;
+    [SerializeField] public TriggerScript_PlayerCheck triggerObj4;
+
+    private PushPull_Remake self;
+
     public PushPullMaster Master;
 
     public bool currentlyActive;
@@ -43,10 +65,30 @@ public class PushPull_Remake : MonoBehaviour
     public bool active;
     bool objectFollow;
 
-    //public int activeObjects = 0;
+    public int addedHit1;
+    public int addedHit2;
+    public int addedHit3;
+    public int addedHit4;
 
-    //public float distance;
-    //public float nearestDistance = 1000000;
+    public bool ray1_1 = false;
+    public bool ray1_2 = false;
+    public bool ray1_3 = false;
+
+
+    public bool ray2_1 = false;
+    public bool ray2_2 = false;
+    public bool ray2_3 = false;
+
+
+    public bool ray3_1 = false;
+    public bool ray3_2 = false;
+    public bool ray3_3 = false;
+
+
+    public bool ray4_1 = false;
+    public bool ray4_2 = false;
+    public bool ray4_3 = false;
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -56,7 +98,7 @@ public class PushPull_Remake : MonoBehaviour
         playerManager = Player.GetComponent<PlayerManager>();
         Movement = Player.GetComponent<PlayerMovement>();
         moveInput = Player.GetComponent<PlayerInput>();
-        //self = MoveableObj.GetComponent<PushPull_Remake>();
+        self = MoveableObj.GetComponent<PushPull_Remake>();
 
         rb = MoveableObj.GetComponent<Rigidbody>();
 
@@ -65,16 +107,26 @@ public class PushPull_Remake : MonoBehaviour
         Interact = moveInput.actions.FindAction("Interact");
 
         newDirection = Direction.transform;
+
+        hitObj1 = self.gameObject;
+        hitObj2 = self.gameObject;
+        hitObj3 = self.gameObject;
+        hitObj4 = self.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (currentTrigger.gameObject.activeSelf == false)
+        {
+            currentTrigger.inTrigger = false;
+        }
         //AllPushObjects();
         if (active)
         {
             TriggerCheck();
             PushPulling();
+
             Movement.pushpulling = pushPulling;
             if (pushPulling)
             {
@@ -106,6 +158,22 @@ public class PushPull_Remake : MonoBehaviour
         TriggerCheck();
     }
 
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            active = false;
+        }
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            active = true;
+        }
+    }
+
     void FixedUpdate() //for collision detection
     {
         if (active)
@@ -114,6 +182,26 @@ public class PushPull_Remake : MonoBehaviour
             if (objectFollow && moveAction2.IsInProgress() && pushPulling)
             {
                 rb.MovePosition(transform.position + Movement.move * Time.deltaTime);
+                if (triggerObj1.inTrigger)
+                {
+                    rb.AddForce(transform.forward + Movement.move * Time.deltaTime);
+                }
+
+                if (triggerObj2.inTrigger)
+                {
+                    rb.AddForce(transform.right + Movement.move * Time.deltaTime);
+                }
+
+                if (triggerObj3.inTrigger)
+                {
+                    rb.AddForce(-transform.forward + Movement.move * Time.deltaTime);
+                }
+
+                if (triggerObj4.inTrigger)
+                {
+                    rb.AddForce(-transform.right + Movement.move * Time.deltaTime);
+                }
+
             }
 
             if(Interact.IsPressed() && pushPulling)
@@ -133,12 +221,451 @@ public class PushPull_Remake : MonoBehaviour
                 Player.transform.rotation = newRot;
             }
         }
-        /*PushPulling();
-        if (objectFollow && moveAction2.IsInProgress() && pushPulling)
+
+
+        /////////////////////////////////////////////////////////////////////////
+        // Raycast //
+        /////////////////////////////////////////////////////////////////////////
+
+        foreach (var objs in Ray1)
         {
-            rb.MovePosition(transform.position + Movement.move * Time.deltaTime);
+            var ray1 = new Ray(objs.transform.position, objs.transform.forward);
+            RaycastHit hit1;
+            if (Physics.Raycast(ray1, out hit1, range))
+            {
+                hitObj1 = hit1.transform.gameObject;
+                if (hit1.transform.gameObject.CompareTag("Untagged"))
+                {
+                    if (objs == Ray1[0])
+                    {
+                        if (ray1_1 == false)
+                        {
+                            addedHit1++;
+                            ray1_1 = true;
+                        }
+                    }
+                    if (objs == Ray1[1])
+                    {
+                        if (ray1_2 == false)
+                        {
+                            addedHit1++;
+                            ray1_2 = true;
+                        }
+                    }
+                    if (objs == Ray1[2])
+                    {
+                        if (ray1_3 == false)
+                        {
+                            addedHit1++;
+                            ray1_3 = true;
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                Debug.Log("working");
+                //hitObj1 = self.gameObject;
+                if (objs == Ray1[0])
+                {
+                    if(ray1_1 == true)
+                    {
+                        addedHit1--;
+                        ray1_1 = false;
+                    }
+
+                }
+                if (objs == Ray1[1])
+                {
+                    if (ray1_2 == true)
+                    {
+                        addedHit1--;
+                        ray1_2 = false;
+                    }
+
+                }
+                if (objs == Ray1[2])
+                {
+                    if (ray1_3 == true)
+                    {
+                        addedHit1--;
+                        ray1_3 = false;
+                    }
+
+                }
+
+            }
+        }
+
+        foreach (var objs in Ray2)
+        {
+            var ray1 = new Ray(objs.transform.position, objs.transform.forward);
+            RaycastHit hit1;
+            if (Physics.Raycast(ray1, out hit1, range))
+            {
+                hitObj1 = hit1.transform.gameObject;
+                if (hit1.transform.gameObject.CompareTag("Untagged"))
+                {
+                    if (objs == Ray2[0])
+                    {
+                        if (ray2_1 == false)
+                        {
+                            addedHit2++;
+                            ray2_1 = true;
+                        }
+                    }
+                    if (objs == Ray2[1])
+                    {
+                        if (ray2_2 == false)
+                        {
+                            addedHit2++;
+                            ray2_2 = true;
+                        }
+                    }
+                    if (objs == Ray2[2])
+                    {
+                        if (ray2_3 == false)
+                        {
+                            addedHit2++;
+                            ray2_3 = true;
+                        }
+                    }
+                    //triggerObj2.gameObject.SetActive(false);
+                }
+
+            }
+            else
+            {
+                if (objs == Ray2[0])
+                {
+                    if (ray2_1 == true)
+                    {
+                        addedHit2--;
+                        ray2_1 = false;
+                    }
+
+                }
+                if (objs == Ray2[1])
+                {
+                    if (ray2_2 == true)
+                    {
+                        addedHit2--;
+                        ray2_2 = false;
+                    }
+
+                }
+                if (objs == Ray2[2])
+                {
+                    if (ray2_3 == true)
+                    {
+                        addedHit2--;
+                        ray2_3 = false;
+                    }
+
+                }
+                //triggerObj2.gameObject.SetActive(true);
+            }
+        }
+
+        foreach (var objs in Ray3)
+        {
+            var ray1 = new Ray(objs.transform.position, objs.transform.forward);
+            RaycastHit hit1;
+            if (Physics.Raycast(ray1, out hit1, range))
+            {
+                hitObj1 = hit1.transform.gameObject;
+                if (hit1.transform.gameObject.CompareTag("Untagged"))
+                {
+                    if (objs == Ray3[0])
+                    {
+                        if (ray3_1 == false)
+                        {
+                            addedHit3++;
+                            ray3_1 = true;
+                        }
+                    }
+                    if (objs == Ray3[1])
+                    {
+                        if (ray3_2 == false)
+                        {
+                            addedHit3++;
+                            ray3_2 = true;
+                        }
+                    }
+                    if (objs == Ray3[2])
+                    {
+                        if (ray3_3 == false)
+                        {
+                            addedHit3++;
+                            ray3_3 = true;
+                        }
+                    }
+
+                }
+
+            }
+            else
+            {
+                if (objs == Ray3[0])
+                {
+                    if (ray3_1 == true)
+                    {
+                        addedHit3--;
+                        ray3_1 = false;
+                    }
+
+                }
+                if (objs == Ray3[1])
+                {
+                    if (ray3_2 == true)
+                    {
+                        addedHit3--;
+                        ray3_2 = false;
+                    }
+
+                }
+                if (objs == Ray3[2])
+                {
+                    if (ray3_3 == true)
+                    {
+                        addedHit3--;
+                        ray3_3 = false;
+                    }
+
+                }
+            }
+        }
+
+        foreach (var objs in Ray4)
+        {
+            var ray1 = new Ray(objs.transform.position, objs.transform.forward);
+            RaycastHit hit1;
+            if (Physics.Raycast(ray1, out hit1, range))
+            {
+                hitObj1 = hit1.transform.gameObject;
+                if (hit1.transform.gameObject.CompareTag("Untagged"))
+                {
+                    if (objs == Ray4[0])
+                    {
+                        if (ray4_1 == false)
+                        {
+                            addedHit4++;
+                            ray4_1 = true;
+                        }
+                    }
+                    if (objs == Ray4[1])
+                    {
+                        if (ray4_2 == false)
+                        {
+                            addedHit4++;
+                            ray4_2 = true;
+                        }
+                    }
+                    if (objs == Ray4[2])
+                    {
+                        if (ray4_3 == false)
+                        {
+                            addedHit4++;
+                            ray4_3 = true;
+                        }
+                    }
+                    //triggerObj4.gameObject.SetActive(false);
+                }
+
+            }
+            else
+            {
+                if (objs == Ray4[0])
+                {
+                    if (ray4_1 == true)
+                    {
+                        addedHit4--;
+                        ray4_1 = false;
+                    }
+
+                }
+                if (objs == Ray4[1])
+                {
+                    if (ray4_2 == true)
+                    {
+                        addedHit4--;
+                        ray4_2 = false;
+                    }
+
+                }
+                if (objs == Ray4[2])
+                {
+                    if (ray4_3 == true)
+                    {
+                        addedHit4--;
+                        ray4_3 = false;
+                    }
+
+                }
+            }
+        }
+
+        if (addedHit1 >= 1)
+        {
+            triggerObj1.gameObject.SetActive(false);
+        }
+        else
+        {
+            triggerObj1.gameObject.SetActive(true);
+            addedHit1 = 0;
+        }
+
+        if (addedHit2 >= 1)
+        {
+            triggerObj2.gameObject.SetActive(false);
+        }
+        else
+        {
+            triggerObj2.gameObject.SetActive(true);
+            addedHit2 = 0;
+        }
+
+        if (addedHit3 >= 1)
+        {
+            triggerObj3.gameObject.SetActive(false);
+        }
+        else
+        {
+            triggerObj3.gameObject.SetActive(true);
+            addedHit3 = 0;
+        }
+
+        if (addedHit4 >= 1)
+        {
+            triggerObj4.gameObject.SetActive(false);
+        }
+        else
+        {
+            triggerObj4.gameObject.SetActive(true);
+            addedHit4 = 0;
+        }
+
+        ///1///
+        /*
+        var ray1 = new Ray(Ray1.transform.position, Ray1.transform.forward);
+        RaycastHit hit1;
+        if (Physics.Raycast(ray1, out hit1, range, LayerMask.GetMask("Default")))
+        {
+            Debug.DrawRay(Ray2.transform.position, Ray2.transform.forward, Color.green);
+            hitObj1 = hit1.transform.gameObject;
+            if (hit1.transform.gameObject.CompareTag("Environment"))
+            {
+                triggerObj1.gameObject.SetActive(false);
+            }
+
+        }
+        else
+        {
+            Debug.Log("working");
+            //hitObj1 = self.gameObject;
+            triggerObj1.gameObject.SetActive(true);
+        }
+
+        ///2///
+
+        var ray2 = new Ray(Ray2.transform.position, Ray2.transform.forward);
+        RaycastHit hit2;
+        if (Physics.Raycast(ray2, out hit2, range, LayerMask.GetMask("Default")))
+        {
+            Debug.DrawRay(Ray2.transform.position, Ray2.transform.forward, Color.green);
+            hitObj2 = hit2.transform.gameObject;
+            if (hit2.transform.gameObject.CompareTag("Environment"))
+            {
+                triggerObj2.gameObject.SetActive(false);
+            }
+
+        }
+        else
+        {
+            Debug.Log("working");
+            //hitObj2 = self.gameObject;
+            triggerObj2.gameObject.SetActive(true);
+        }
+
+        ///3///
+
+        var ray3 = new Ray(Ray3.transform.position, Ray3.transform.forward);
+        RaycastHit hit3;
+        if (Physics.Raycast(ray3, out hit3, range, LayerMask.GetMask("Default")))
+        {
+            Debug.DrawRay(Ray3.transform.position, Ray3.transform.forward, Color.green);
+            hitObj3 = hit3.transform.gameObject;
+            if (hit3.transform.gameObject.CompareTag("Environment"))
+            {
+                triggerObj3.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            Debug.Log("working");
+            //hitObj3 = self.gameObject;
+            triggerObj3.gameObject.SetActive(true);
+        }
+
+        ///4///
+
+        var ray4 = new Ray(Ray4.transform.position, Ray4.transform.forward);
+        RaycastHit hit4;
+        if (Physics.Raycast(ray4, out hit4, range, LayerMask.GetMask("Default")))
+        {
+            Debug.DrawRay(Ray4.transform.position, Ray4.transform.forward, Color.green);
+            hitObj4 = hit4.transform.gameObject;
+            if (hit4.transform.gameObject.CompareTag("Environment"))
+            {
+                triggerObj4.gameObject.SetActive(false);
+            }
+
+        }
+        else
+        {
+            Debug.Log("working");
+            //hitObj4 = self.gameObject;
+            triggerObj4.gameObject.SetActive(true);
         }*/
-        //rb.MovePosition(transform.position + Movement.move * Time.deltaTime);
+
+        /////// MAKE THEM SEPERATE!!! IT WILL FIX THE PROBLEM!!!///////
+
+        /*foreach (var Rays in Raycasts)
+        {
+            var ray = new Ray(Rays.transform.position, Rays.transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, range))
+            {
+                hitObj = hit.transform.gameObject;
+                if (hitObj.CompareTag("Environment"))
+                {
+                    if (!hasHit)
+                    {
+                        hasHit = true;
+                    }
+                    //triggerObj.gameObject.SetActive(false);
+                }
+
+                if (hasHit)
+                {
+                    triggerObj = Rays.gameObject.GetComponentInChildren<TriggerScript_PlayerCheck>();
+                    triggerObj.gameObject.SetActive(false);
+                }
+
+            }
+            else
+            {
+                if (hasHit)
+                {
+                    hasHit= false;
+                }
+                if (!hasHit)
+                {
+                    triggerObj.gameObject.SetActive(true);
+                }
+            }
+        }*/
     }
 
     void PushPulling()
@@ -182,7 +709,7 @@ public class PushPull_Remake : MonoBehaviour
                     newRot = currentTrigger.transform.rotation;
                     var pos = new Vector3 (currentTrigger.transform.position.x, 1.58f, currentTrigger.gameObject.transform.position.z);
                     Player.transform.rotation = newRot;
-                    Player.transform.position = pos;
+                    Player.transform.localPosition = pos;
                     //Player.transform.position = 
                     Interact.Reset();
                     //MoveableObj.transform.parent = Player.transform;
@@ -203,6 +730,7 @@ public class PushPull_Remake : MonoBehaviour
                 playerManager.moveAction = moveAction;
                 Debug.Log("Reset");
             }
+
             //Movement.PushPullStop();
             //playerManager.moveAction = moveAction;
             //Debug.Log("Reset");
