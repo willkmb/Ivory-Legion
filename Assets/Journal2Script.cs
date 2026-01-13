fb94486b1b5bc3fe23cd5e955b9a31d3a1de4b15
willkmb <thewillkimber@gmail.com>
2026-01-13 13:00:28 +0000

    journal

--- Assets/Journal2Script.cs
+++ Assets/Journal2Script.cs
@@ -5,10 +5,20 @@ public class Journal2Script : MonoBehaviour
 {
     [SerializeField] GameObject holder;
     [SerializeField] GameObject icon;
+    private List<GameObject> entries = new List<GameObject>();
     private int cap = 15;
+    private int index = 0;
 
     private void Awake()
     {
-        for(int i = 0; i < cap; i++) { Instantiate(icon, holder.transform); }
+        for(int i = 0; i < cap; i++) { GameObject cur = Instantiate(icon, holder.transform); entries.Add(cur); }
+    }
+
+    private void Update()
+    {
+        float wheel = Input.mouseScrollDelta.y;
+
+        if (wheel > 0f) { index++; Debug.Log(index); }
+        else if(wheel < 0f) { index--; Debug.Log(index); }
     }
 }
